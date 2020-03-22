e184aa9e9bdb64a2d34c91ff070d087205688bbe
Yu Chi To <yuchitoto@gmail.com>
2020-03-23 00:27:56 +0800

    add test php file

---
+++ server/view/hello_world.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hello world!</title>
+  </head>
+  <body>
+    <h1>Hello world!</h1>
+    <p>Well, this html is to setup the html folder on Github.</p>
+    <?php
+    echo "some php script";
+    if(false) {
+    ?>
+      <p><strong>Why the hell should php be used?</strong></p>
+    <?php } else { ?>
+      <p>I <b>love</b> PHP</p>
+    <?php }?>
+  </body>
+</html>
