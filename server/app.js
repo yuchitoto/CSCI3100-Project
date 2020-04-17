d94a373b0cbdb3f61bcb3171ee15a177fc268cb1
YU CHI TO <yuchitoto@gmail.com>
2020-04-17 21:14:12 +0800

    update css handler

--- server/app.js
+++ server/app.js
@@ -227,7 +227,7 @@ app.use('*.png|*.jpg', function(req, res) {
 });
 
 app.use('*.css', function(req, res) {
-  fs.readFile(req._parsedUrl.pathname,function(err, data) {
+  fs.readFile('./'+req._parsedUrl.pathname,function(err, data) {
     if(err)
     {
       console.log(`error: ${err.message}`);
