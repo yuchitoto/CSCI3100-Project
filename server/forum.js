6ef0056afee4ff69ffdcb30a9845d96953146e87
Yu Chi To <yuchitoto@gmail.com>
2020-04-20 12:24:16 +0800

    update forum module

--- server/forum.js
+++ server/forum.js
@@ -36,25 +36,32 @@ class Forum {
 // to search post and return titles
   search(param, callback)
   {
+    var finde = JSON.parse(param);
     /*parse parameter*/
-    var key = "";
+    var key = {exactTitle:finde[exactTitle], existsTitle:finde[existsTitle], user:finde[user], inContext:finde[inContext], exactContext:finde[exactContext]};
     const find = fork("connect_sql.js", ["search_post"]);
     find.send(JSON.stringify(key));
 
     find.on("message", m => {
       /*handle message*/
+      const parsed = JSON.parse(m);
+      if(parsed=='fail')
+      {
+        return callback(1, m);
+      }
+      return callback(0, m);
     });
   }
 
 // post new posts
   post_post(title, content, codeID, callback)
   {
-    var msg = {userid: this.userid, codeID:codeID, title:title, content:content, reply:0};
+    var msg = {USER: this.userid, CODE:codeID, TITLE:title, CONTENT:content, REPLY:0};
     const poster = fork("connect_sql.js", ["new_post"]);
     poster.send(JSON.stringify(msg));
     poster.on("message", m => {
       var tmp = JSON.parse(m);
-      callback(m);
+      return callback(m);
     });
   }
 
@@ -62,12 +69,29 @@ class Forum {
   post_reply(postID, content, callback)
   {
     /*send to mysql*/
+    const data = {
+      REPLY:postID,
+      USER:this.userID,
+      CONTENT:content
+    };
+    const eng = fork("connect_sql.js", ["new_post"]);
+    eng.send(JSON.parse(data));
+    eng.on("message", msg => {return callback(msg);});
   }
 
 // delete reply or post
   delete(param, callback)
   {
     /*delete reply or post*/
+  }
+
+  titles(callback)
+  {
+    const eng = fork("connect_sql.js", ["search_post_head"]);
+    eng.send(JSON.parse({existsTitle:[],user:[],inContext:[],id:[]}));
+    eng.on("message", msg => {
+      return callback(msg);
+    });
   }
 
   /*advanced
